2dbfc9724a91abae92347866ed900265b6d9b47a
Justin Cheung <jrcheung168@gmail.com>
2025-07-20 00:58:46 -0500

    Run from anywhere

--- pull.py
+++ pull.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 import argparse
+import os
+import sys
 
-import src.puller
 import config
+import src.puller
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
 
 def main():
     parser = argparse.ArgumentParser(
