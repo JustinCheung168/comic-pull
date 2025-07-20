ca4358b208ad54866fc7a0e8b8234a4dc2be6d60
Justin Cheung <jrcheung168@gmail.com>
2025-07-19 22:19:25 -0500

    Fix pull list script

--- pull_list.sh
+++ pull_list.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+
+# Put your pull commands here:
 ./pull.py superman-smashes-the-klan-2019
 ./pull.py supergirl-woman-of-tomorrow-2021
 ./pull.py absolute-batman-2024 --first-issue 8
