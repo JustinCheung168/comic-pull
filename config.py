758d53d7f607bf3d15263444b1ef3c72eabe2429
Justin Cheung <jrcheung168@gmail.com>
2025-07-25 20:58:02 -0500

    Clarify config

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
-DEFAULT_LIBRARY_PATH="/Users/justincheung/Data/Comics"
-DEFAULT_SOURCE_URL='readcomicsonline.ru'
+
+# Path to the directory where downloaded comics will be saved by default.
+DEFAULT_LIBRARY_PATH = "/Users/justincheung/Data/Comics"
+
+# Default source website for downloading comics. If you change this site, then you must define behavior for downloading from this site in `src/sources.py`.
+DEFAULT_SOURCE_URL = 'readcomicsonline.ru'
